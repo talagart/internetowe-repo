f9d8a2a92a6ef5e2262c92861ba94026a3bd0257
talagart <rtalaga92@gmail.com>
2020-05-21 09:33:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+numbers = list(range(1, 51))
+print(numbers, "\n")
+numbers.extend(list(range(51, 61)))
+print(numbers, "\n")
+
+multiple_of_2 = [n for n in numbers if n % 2 == 0]
+print(multiple_of_2, "\n")
+
+multiple_of_3 = [n for n in numbers if n % 3 == 0]
+print(multiple_of_3, "\n")
